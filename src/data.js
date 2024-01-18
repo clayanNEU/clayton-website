922e94aa2f25902317edf000cd9c84b2d99d72ab
Clayton Yan <yan.cl@northeastern.edu>
2024-01-18 14:27:54 -0800

    added position data

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const positions = [
+  {
+    id: 0,
+    company: "ASICS Digital",
+    profession: "Software Engineering co-op",
+    accomplishment: "Created APIs",
+    imageId: "MK3eW3A",
+  },
+  {
+    id: 1,
+    company: "Seaport Diagnostics",
+    profession: "Software Engineering co-op",
+    accomplishment: "Data Dashboard using APIs",
+    imageId: "MK3eW3A",
+  },
+  {
+    id: 2,
+    company: "Enel X",
+    profession: "Application Support co-op",
+    accomplishment: "Technical ops in internal and external website",
+    imageId: "MK3eW3A",
+  },
+];
